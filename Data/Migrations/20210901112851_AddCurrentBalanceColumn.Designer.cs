3c84155cc673ded6cd523f620f631b0e994fe870
Ivan Cherepanov <ivan.cherepanov@dptechnology.com>
2021-09-01 18:37:46 +0700

    adding migration

--- Data/Migrations/20210901112851_AddCurrentBalanceColumn.Designer.cs
+++ Data/Migrations/20210901112851_AddCurrentBalanceColumn.Designer.cs
@@ -4,14 +4,16 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Data.Migrations
 {
     [DbContext(typeof(LoymaxTestContext))]
-    partial class LoymaxTestContextModelSnapshot : ModelSnapshot
+    [Migration("20210901112851_AddCurrentBalanceColumn")]
+    partial class AddCurrentBalanceColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20210901112851_AddCurrentBalanceColumn.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddCurrentBalanceColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // In real project I would add and updated CurrentBalance directly in DBMS when server is off, not in migrations
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "CurrentBalance",
+                table: "Accounts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentBalance",
+                table: "Accounts");
+        }
+    }
+}
